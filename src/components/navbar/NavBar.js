66ca74d1c544ffa93063d2454f03ff4a6d80f0f3
Pilar Elisii <pilarelisii5@gmail.com>
2021-10-13 22:17:09 -0300

    commit

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,23 @@
+import './NavBar.css';
+
+export function Navbar(){
+    return (
+    <>
+    <header className="contenedor-header">
+            <nav className="header-nav">
+                <div className="logo-brand">
+                   <a href="#"> <img src="" alt="LOGO"/> </a>
+                </div>
+                <div className="menu">
+                    <ul>
+                        <a href="#"><li>Inicio</li></a>
+                        <a href="#"><li>Productos</li></a>
+                        <a href="#"><li>Preguntas Frecuentes</li></a>
+                        <a href="#"><li>Contacto</li></a>
+                    </ul>
+                </div>
+            </nav>
+    </header>
+    </>
+    );
+}
